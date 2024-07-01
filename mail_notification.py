03d1cc1acbf9c371806993be7aae70aec5d7be6c
Zack <wang.zack@inventec.com>
2024-07-01 11:33:35 +0800

    Add files for outlook notification

---
+++ mail_notification.py
@@ -0,0 +1,20 @@
+import win32com.client as win32
+
+def send_email(subject, body, recipients):
+    try:
+        outlook = win32.Dispatch('Outlook.Application')
+        mail = outlook.CreateItem(0)
+        mail.Subject = subject
+        mail.Body = body
+        mail.To = recipients
+        mail.Send()
+        print("Pass！")
+    except Exception as e:
+        print("Fail:", str(e))
+
+subject = "(Test)Your signed files are ready"
+body = "Your Microsoft cab sign is ready."
+recipients = "wang.zack@inventec.com"
+
+
+send_email(subject, body, recipients)
---
+++ monitor.ps1
@@ -0,0 +1,23 @@
+$folderPath = "C:\Cabby\Folder"
+$scriptPath = "C:\Cabby\mail_notification.py"
+
+$filter = '*.*'
+
+$fsw = New-Object IO.FileSystemWatcher $folderPath, $filter
+$fsw.EnableRaisingEvents = $true
+$fsw.IncludeSubdirectories = $false
+
+$action = {
+    $path = $Event.SourceEventArgs.FullPath
+    $changeType = $Event.SourceEventArgs.ChangeType
+    Write-Host "File ${changeType}: ${path}"
+    # Ensure Python is correctly invoked with the script
+    & python $scriptPath
+}
+
+Register-ObjectEvent $fsw 'Created' -Action $action
+
+Write-Host "Monitoring folder: $folderPath"
+while ($true) {
+    Start-Sleep -Seconds 5
+}
